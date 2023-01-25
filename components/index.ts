c0871ce3e8028b828880a6001f0d1ac3090f3896
kobak-github <konurbaevbm@gmail.com>
2023-01-25 11:59:39 +0600

    layout products page

--- components/index.ts
+++ components/index.ts
@@ -3,4 +3,7 @@ export * from './Tag/Tag';
 export * from './P/P';
 export * from './Button/Button';
 export * from './Rating/Rating';
+export * from './Card/Card';
+export * from './HhData/HhData';
+export * from './Advantages/Advantages';
 
