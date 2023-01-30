514085a98d3a3c70c7eabb2fbd82b52bc5b09f57
kobak-github <konurbaevbm@gmail.com>
2023-01-31 00:26:20 +0600

    Sort span to button

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import { SortEnum, SortProps } from './Sort.props'
-import styles from './Sort.module.css'
-import SortIcon from './sort.svg'
-import cn from 'classnames'
+import React from 'react';
+import { SortEnum, SortProps } from './Sort.props';
+import styles from './Sort.module.css';
+import SortIcon from './sort.svg';
+import cn from 'classnames';
 
-export const Sort = ({sort, setSort, className, ...props}: SortProps): JSX.Element => {
+export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
   return (
     <div className={cn(styles.sort, className)} {...props}>
-      <span
+      <button
         onClick={() => setSort(SortEnum.Rating)}
         className={cn({
           [styles.active]: sort == SortEnum.Rating,
-        })}
-      >
-        <SortIcon className={styles.sortIcon}/>По рейтингу
-      </span>
-      <span
+        })}>
+        <SortIcon className={styles.sortIcon} />
+        По рейтингу
+      </button>
+      <button
         onClick={() => setSort(SortEnum.Price)}
         className={cn({
           [styles.active]: sort == SortEnum.Price,
-        })}
-      >
-        <SortIcon className={styles.sortIcon}/>По цене
-      </span>
+        })}>
+        <SortIcon className={styles.sortIcon} />
+        По цене
+      </button>
     </div>
-  )
-}
+  );
+};
